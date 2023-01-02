71fe5532f24846c3da0d6205862f23b673609848
Jasureb <jasurisoqov685@gmail.com>
2023-01-02 22:22:43 +0500

    Lesson 1: Variables and Data Types

---
+++ script.js
@@ -0,0 +1,82 @@
+// Lesson 1: Variables
+
+// let variable = "Variable";
+
+// let firstName = "Jasur",
+//   lastName = "Isokov",
+//   age = "20",
+//   university = "TUIT";
+
+// console.log(age);
+
+// let x = (y = z = 21);
+
+// console.log(z);
+
+// -------------------------------------
+
+// Data types
+
+// 1 | Primitive
+
+// Number
+
+// ---------------------
+
+// Bigint
+
+// let x = 173129730192710923710293710937102938n + 112312n;
+
+// ---------------------
+
+// String " ", ' ', ` ` ;
+
+// let fName = "Jasurbek";
+
+// console.log(fName + " Isokov");
+
+// ---------------------
+
+// Boolean true and false
+
+// ---------------------
+
+// Null
+
+// ---------------------
+
+// Undefined
+
+// let a;
+// let b = null;
+// let c = 0;
+
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a == c);
+// console.log(typeof a, typeof b);
+
+// ---------------------
+
+// Symbols
+
+// let a = Symbol("text");
+// let b = Symbol("text");
+
+// console.log(a, b);
+// console.log(a == b);
+// console.log(a === b);
+
+// -----------------------------------------
+
+// 2 | Non-Primitive
+
+// Object => object, array, function...\
+
+// -----------------------------------------
+
+// alert("salom");
+// prompt("Yoshingizni kiriting");
+// confirm("Yoshingiz");
+
+// ========================================================================================
